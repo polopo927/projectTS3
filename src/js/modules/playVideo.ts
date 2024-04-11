82be9e783671d08cbffc649e0e55e2848a7b70a0
polopo927 <polopo927@mail.ru>
2024-04-11 15:40:09 +0300

    видеоплеер

---
+++ src/js/modules/playVideo.ts
@@ -0,0 +1,72 @@
+declare const YT: any
+/* declare function onPlayerReady(event: any): void
+declare function onPlayerStateChange(event: any): void */
+
+
+export class VideoPlayer {
+	btns: NodeListOf<HTMLElement>
+	overlay: HTMLElement | null
+	close: Element | null
+	player: YT.Player | undefined
+	constructor(triggers: string, overlaySelector: string) {
+		this.btns = document.querySelectorAll(triggers)
+		this.overlay = document.querySelector(overlaySelector)
+		this.close = this.overlay ? this.overlay.querySelector('.close') : null
+	}
+
+	bindTriggers() {
+		this.btns.forEach(btn => {
+			btn.addEventListener('click', () => {
+				const path = btn.getAttribute('data-url')
+				if (path) {
+					this.createPlayer(path)
+				}
+			})
+		})
+	}
+
+	bindCloseBtn() {
+		this.close?.addEventListener('click', () => {
+			if (this.overlay) {
+				this.overlay.style.display = 'none'
+				if (this.player && 'stopVideo' in this.player) {
+					this.player?.stopVideo()
+				}
+			}
+		})
+	}
+
+	createPlayer(url: string) {
+		if (!this.player) {
+			this.player = new YT.Player('frame', {
+				height: '100%',
+				width: '100%',
+				videoId: `${url}`
+				/* playerVars: {
+					'playsinline': 1
+				},
+				events: {
+					'onReady': onPlayerReady,
+					'onStateChange': onPlayerStateChange
+				} */
+			});
+		}
+		if (this.overlay) {
+			this.overlay.style.display = 'flex'
+		}
+	}
+
+	init() {
+		const tag = document.createElement('script');
+
+		tag.src = "https://www.youtube.com/iframe_api";
+		const firstScriptTag = document.getElementsByTagName('script')[0];
+		const parentElement = firstScriptTag.parentNode
+		if (parentElement) {
+			parentElement.insertBefore(tag, firstScriptTag);
+		}
+
+		this.bindTriggers()
+		this.bindCloseBtn()
+	}
+}
